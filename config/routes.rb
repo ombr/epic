f31bf5a37c2c2fd7841653813bcd7ecb631375ff
Luc <luc@boissaye.fr>
2014-07-31 13:07:02 +0200

    Merge origin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 require 'resque_web'
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   default_url_options host: ENV['DOMAIN']
-  # authenticated :user do
+  authenticated :user do
+    ResqueWeb::Engine.eager_load! if Rails.env.development?
     mount ResqueWeb::Engine => '/resque'
-  # end
+  end
   resources :images, only: [:show, :create, :index, :new] do
     collection do
       get :add
